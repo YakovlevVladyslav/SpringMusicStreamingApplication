a804e9dd78fb6448108636d8508ff2e46d3a1982
YakovlevVladyslav <yakovlevvladyslav6@gmail.com>
2026-02-28 20:07:36 +0200

    fresh start

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpringMusicStreamingApplication
+Realization of music streaming app with Java Spring
+
---
+++ src/main/java/com/example/app/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
